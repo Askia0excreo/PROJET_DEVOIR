65ef9b1bfcf8a30c647ebb7dd1342cc137a3fc2a
Askia0excreo <imanyxformation@gmail.com>
2025-05-30 09:57:13 +0000

    Ajout des tables

---
+++ resources/views/layouts/structure.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('titre')</title>
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>

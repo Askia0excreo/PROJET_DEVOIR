a1bdd8eabf4a000ed4dd9acf18c43e0eec9858ec
Askia0excreo <imanyxformation@gmail.com>
2025-05-30 10:59:17 +0000

    Ajout de Admin lte et des views

--- app/Http/Controllers/etudiantController.php
+++ app/Http/Controllers/etudiantController.php
@@ -11,7 +11,7 @@ class etudiantController extends Controller
      */
     public function index()
     {
-        //
+        return view('etudiant.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/etudiant', [App\Http\Controllers\etudiantController::class, 'index'])->name('etudiant.index');
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+
